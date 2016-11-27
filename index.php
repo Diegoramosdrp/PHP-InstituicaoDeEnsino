c6e1fd058411f3060146764329b116f9ad0d61af
Diegoramosdrp <Diegoramosdrp@gmail.com>
2016-11-27 01:32:04 -0200

    Criado Template Padrao Para Todas As Paginas Inclusao De Menu V1.0

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <title>UniversityExpert</title>
+    </head>
+    <body>
+        <?php 
+            include 'Template.php';
+        ?>
+    </body>
+</html>
